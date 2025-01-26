4f46d0587467d13cdb21ca06e41a983c7a373b1f
francesco-scar <francesco_git@protonmail.com>
2025-01-26 17:58:35 +0100

    :chart_with_upwards_trend: Aggiornamento per media mobile (da decidere un po' meglio)

--- script/variance_average_plot.py
+++ script/variance_average_plot.py
@@ -12,11 +12,12 @@ json_file = sys.argv[1]
 f = open(json_file, 'r')
 data = json.load(f)
 
+print(f"Found {len(data)} repetitions")
+
 time = []
 stat_data = []
 
 for repetition in data:
-    print(repetition)
     for vector in data[repetition]['vectors']:
         if vector['name'] != stat:
             continue
@@ -34,7 +35,7 @@ resampled_time.sort()
 resampled_stat_data = [[] for i in range(len(stat_data))]
 variance = []
 average = []
-
+moving_average = []
 
 # resample data as a step function
 for i in range(len(stat_data)):
@@ -44,14 +45,35 @@ for i in range(len(stat_data)):
             j += 1
         resampled_stat_data[i].append(stat_data[i][j])
 
+print(f"Resampled data with {len(resampled_time)} points")
+
 for i in range(len(resampled_time)):
     average.append(sum([resampled_stat_data[j][i] for j in range(len(resampled_stat_data))]) / len(resampled_stat_data))
+
+print(f"Computed average")
 
 for i in range(len(resampled_time)):
     variance.append(sum([(resampled_stat_data[j][i] - average[i])**2 for j in range(len(resampled_stat_data))]) / len(resampled_stat_data))
 
+print(f"Computed variance")
+
+WINDOW_SIZE = 100 # in seconds
+right_index = 0
+for left_index in range(len(resampled_time) - WINDOW_SIZE):
+    continue   
+    # set right index to the last time <= time_window
+    while right_index < len(resampled_time)-1 and resampled_time[right_index] - resampled_time[left_index] < WINDOW_SIZE:
+        right_index += 1
+    actual_window_size = resampled_time[right_index] - resampled_time[left_index]
+    moving_average.append(sum(average[left_index:right_index]) / actual_window_size)
+    
+    #print(left_index, right_index, actual_window_size, end='\r')
+
+print(f"Computed moving average with window size {WINDOW_SIZE} seconds")
+
 # plot average with variance confidence interval at 95%
 plt.plot(resampled_time, average, label='Average')
+#plt.plot(resampled_time[:len(moving_average)], moving_average, label=f'Moving Average with window size {WINDOW_SIZE} seconds')
 plt.fill_between(resampled_time, [average[i] - 1.96 * variance[i] for i in range(len(variance))], [average[i] + 1.96 * variance[i] for i in range(len(variance))], alpha=0.5, label='95% Confidence Interval')
 
 # final expected value line
